2afaa5f1094f5f555f766db5b2cde720333ae45d
junioranheu <junioranheu@gmail.com>
2022-01-25 15:43:17 -0300

    Router

---
+++ src/views/dashboard/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Index() {
+    return (
+        <div className='container'>
+            <div className='row min-vh-100'>
+                <div className='col d-flex flex-column justify-content-center align-items-center'>
+                    <div>
+                        <h1 className='text-center'>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
