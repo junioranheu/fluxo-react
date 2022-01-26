9cba0a5e68a113a4e981729dd33582ae65420671
junioranheu <junioranheu@gmail.com>
2022-01-26 09:33:42 -0300

    Padronizar nomes dos arquivos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Estabelecimento from './views/estabelecimento/index2';
+import Estabelecimento from './views/estabelecimento';
 import Entrar from './views/home/entrar';
 import Dashboard from './views/home/index';
 
