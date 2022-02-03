c720a0ee170180cd9d564caeebb2597196e19d19
junioranheu <junioranheu@gmail.com>
2022-02-03 20:37:32 -0300

    fluxo.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { UsuarioContext } from './utilidades/context/usuarioContext';
 import Estabelecimento from './views/estabelecimento/estabelecimentos';
 import GerenciarEstabelecimento from './views/estabelecimento/gerenciarEstabelecimentos';
+import Fluxo from './views/home/fluxo';
 import Inicio from './views/home/index';
 import Politica from './views/home/politica';
 import Reportar from './views/home/reportar';
@@ -22,6 +23,7 @@ export default function App() {
       <Route path='/reportar-problema' element={<Reportar />} />
       <Route path='/sem-acesso' element={<SemAcesso />} />
       <Route path='/entrar' element={<Entrar />} />
+      <Route path='/fluxo' element={<Fluxo />} />
       <Route path='/criar-conta' element={!isAuth ? <CriarConta /> : <Navigate to={'/sem-acesso'} />} />
       <Route path='/gerenciar-estabelecimentos' element={isAuth ? <GerenciarEstabelecimento /> : <Navigate to={'/sem-acesso'} />} />
       <Route path='/estabelecimento/tipo/:tipoEstabelecimentoId' element={<Estabelecimento />} />
---
+++ src/views/home/fluxo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Fluxo() {
+    return (
+        <div className='mt-4'>
+            <div className='has-text-centered'>
+                <h2>
+                    Fluxo
+                </h2>
+            </div>
+        </div>
+    );
+}
+
