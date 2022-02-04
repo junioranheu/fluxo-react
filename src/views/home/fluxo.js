26202288709c3200804ac73c6fe9985b1e2609f5
junioranheu <junioranheu@gmail.com>
2022-02-04 15:17:31 -0300

    Fluxo.js e ajuste em index.html

--- src/views/home/fluxo.js
+++ src/views/home/fluxo.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import '../../css/landingPage/landingPage.css';
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import '../../css/landingPage/default.css';
+import '../../css/landingPage/style.css';
 import Hamburguer from '../../static/gifs/hamburguer.webp';
 import Pikachu from '../../static/gifs/pikachu.webp';
 import Poze from '../../static/gifs/poze.webp';
@@ -9,90 +13,91 @@ import Relaxando from '../../static/gifs/relaxando.webp';
 import Top from '../../static/gifs/top.webp';
 
 export default function Fluxo() {
+    const [nomeApp] = useState('Fluxo');
+    const [sloganApp] = useState('Conheça sua cidade!');
+
+    useEffect(() => {
+        document.title = 'Fluxo — Bem-vindo!';
+    }, []);
+
+    const configuracaoSlider = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    };
+
     return (
         <React.Fragment>
-            <section id='home' className='slider-area pt-180 pb-30'>
-                <div className='container-fluid position-relative'>
-                    <div className='slider-active'>
-                        {/* <!-- Primeiro slide --> */}
-                        <div className='single-slider'>
-                            <div className='slider-bg'>
-                                <div className='row no-gutters align-items-center'>
-                                    <div className='col-lg-4 col-md-5'>
-                                        <div className='slider-product-image d-none d-md-block'>
-                                            <img src={Hamburguer} className='borda' alt='' />
-                                        </div>
-                                    </div>
+            <section className='slider-area pt-100 pb-30'>
 
-                                    <div className='col-lg-8 col-md-7'>
-                                        <div className='slider-product-content'>
-                                            <h1 className='slider-title mb-25' data-animation='fadeInUp' data-delay='0.3s'>Bem-vindo ao <span>Fluxo</span></h1>
-                                            <a className='main-btn mt-2' href='/criar-conta' data-animation='fadeInUp' data-delay='1.5s'>Crie sua conta <i className='lni-chevron-right'></i></a>
-                                            <a className='main-btn main-btn-2 mt-2' href='/entrar' data-animation='fadeInUp' data-delay='1.9s'>Entre agora <i className='lni-chevron-right'></i></a>
-                                        </div>
+                {/* <!-- Primeiro slide --> */}
+                <Slider {...configuracaoSlider}>
+                    <div className='single-sliderxxx'>
+                        <div className='slider-bg'>
+                            <div className='row no-gutters align-items-center'>
+                                <div className='col-lg-4 col-md-5'>
+                                    <div className='slider-product-image d-none d-md-block'>
+                                        <img src={Hamburguer} className='borda' alt='' />
                                     </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        {/* <!-- Segundo slide --> */}
-                        <div className='single-slider'>
-                            <div className='slider-bg'>
-                                <div className='row no-gutters align-items-center'>
-                                    <div className='col-lg-4 col-md-5'>
-                                        <div className='slider-product-image d-none d-md-block'>
-                                            <img src={Pikachu} className='borda' alt='' />
-
-                                            <div className='slider-discount-tag'>
-                                                <p>Oi, né?</p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className='col-lg-8 col-md-7'>
-                                        <div className='slider-product-content'>
-                                            <h1 className='slider-title mb-10' data-animation='fadeInUp' data-delay='0.3s'>Talvez, <span>você</span>...</h1>
-                                            <p className='mb-25' data-animation='fadeInUp' data-delay='0.9s'>queira @titulo... tá esperando o quê?</p>
-                                            <a className='main-btn' href='/' data-animation='fadeInUp' data-delay='1.5s'>@sloganApp <i className='lni-chevron-right'></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* <!-- Terceiro slide --> */}
-                        <div className='single-slider'>
-                            <div className='slider-bg'>
-                                <div className='row no-gutters align-items-center'>
-                                    <div className='col-lg-4 col-md-5'>
-                                        <div className='slider-product-image d-none d-md-block'>
-                                            <img src={Relaxando} className='borda' alt='' />
-                                        </div>
-                                    </div>
-
-                                    <div className='col-lg-8 col-md-7'>
-                                        <div className='slider-product-content'>
-                                            <h1 className='slider-title mb-10' data-animation='fadeInUp' data-delay='0.3s'><span>Fluxo</span> & <span>relaxamento</span></h1>
-                                            <p className='mb-25' data-animation='fadeInUp' data-delay='0.9s'>Clique aqui embaixo e entre agora mesmo!</p>
-                                            <a className='main-btn' href='/' data-animation='fadeInUp' data-delay='1.5s'>@sloganApp <i className='lni-chevron-right'></i></a>
-                                        </div>
+                                <div className='col-lg-8 col-md-7'>
+                                    <div className='slider-product-content'>
+                                        <h1 className='slider-title mb-25 animate__animated animate__fadeInUp delay03'>Bem-vindo ao <span>{nomeApp}!</span></h1>
+                                        <a className='main-btn mt-2 animate__animated animate__fadeInUp delay15' href='/criar-conta'>Crie sua conta <i className='lni-chevron-right'></i></a>
+                                        <a className='main-btn main-btn-2 mt-2 ml-3 animate__animated animate__fadeInUp delay19' href='/entrar'>Entre agora <i className='lni-chevron-right'></i></a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className='slider-social'>
-                        <div className='row justify-content-end'>
-                            <div className='col-lg-7 col-md-6'>
-                                <ul className='social text-right'>
-                                    <li><a href='#' target='_blank'><i className='lni-facebook-filled'></i></a></li>
-                                    <li><a href='#' target='_blank'><i className='lni-instagram'></i></a></li>
-                                </ul>
+                    {/* <!-- Segundo slide --> */}
+                    <div className='single-sliderxxx'>
+                        <div className='slider-bg'>
+                            <div className='row no-gutters align-items-center'>
+                                <div className='col-lg-4 col-md-5'>
+                                    <div className='slider-product-image d-none d-md-block'>
+                                        <img src={Pikachu} className='borda' alt='' />
+                                    </div>
+                                </div>
+
+                                <div className='col-lg-8 col-md-7'>
+                                    <div className='slider-product-content'>
+                                        <h1 className='slider-title mb-10'>Talvez, <span>você</span>...</h1>
+                                        <p className='mb-25'>queira @titulo... tá esperando o quê?</p>
+                                        <a className='main-btn' href='/'>{sloganApp} <i className='lni-chevron-right'></i></a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+
+                    {/* <!-- Terceiro slide --> */}
+                    <div className='single-sliderxxx'>
+                        <div className='slider-bg'>
+                            <div className='row no-gutters align-items-center'>
+                                <div className='col-lg-4 col-md-5'>
+                                    <div className='slider-product-image d-none d-md-block'>
+                                        <img src={Relaxando} className='borda' alt='' />
+                                    </div>
+                                </div>
+
+                                <div className='col-lg-8 col-md-7'>
+                                    <div className='slider-product-content'>
+                                        <h1 className='slider-title mb-10'><span>{nomeApp}</span> & <span>relaxamento</span></h1>
+                                        <p className='mb-25'>Clique aqui embaixo e entre agora mesmo!</p>
+                                        <a className='main-btn' href='/'>{sloganApp} <i className='lni-chevron-right'></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Slider>
             </section>
 
             {/* <!--====== FIM DA PRIMEIRA PARTE ======-->
@@ -110,7 +115,7 @@ export default function Fluxo() {
                                 <div className='product-content'>
                                     <h4 className='content-title mb-15 fundo-texto'>Imagine um texto bem legal aqui!</h4>
 
-                                    <a className='fundo-texto' href='/' style={{ color: '#211F24 !important' }}>
+                                    <a className='fundo-texto' href='/'>
                                         Ver mais <i className='lni-chevron-right'></i>
                                     </a>
                                 </div>
@@ -124,9 +129,9 @@ export default function Fluxo() {
                                 </div>
 
                                 <div className='product-content'>
-                                    <h4 className='content-title mb-15 fundo-texto'>Testando essa bagaça...<br />1, 2, 3</h4>
+                                    <h4 className='content-title mb-15 fundo-texto'>Testando essa bagaça... 1, 2, 3...</h4>
 
-                                    <a className='fundo-texto' href='/' style={{ color: '#211F24 !important' }}>
+                                    <a className='fundo-texto' href='/'>
                                         Ver mais <i className='lni-chevron-right'></i>
                                     </a>
                                 </div>
@@ -136,7 +141,7 @@ export default function Fluxo() {
                 </div>
             </section>
 
-            <section id='sobre' className='services-area pt-100 pb-200'>
+            <section className='services-area pt-100 pb-200xxx'>
                 <div className='container'>
                     <div className='row'>
                         <div className='col-lg-6'>
@@ -144,7 +149,7 @@ export default function Fluxo() {
                                 <h5 className='mb-15 sem-highlight'>!!!</h5>
                                 <h3 className='title mb-15'>Está perdido?</h3>
                                 <p>
-                                    Conheça sua cidade usando o Fluxo<br />
+                                    Conheça sua cidade usando o {nomeApp}<br />
                                     Descubra nov<span id='spanTypeWriter'><span className='Typewriter__wrapper'>ex</span><span className='Typewriter__cursor'>|</span></span>
                                 </p>
                             </div>
@@ -156,7 +161,7 @@ export default function Fluxo() {
                             <div className='services-left mt-45'>
                                 <div className='services'>
                                     <img src={Procurando} className='borda' alt='' />
-                                    <a href='/' className='main-btn mt-30'>@sloganApp <i className='lni-chevron-right'></i></a>
+                                    <a href='/' className='main-btn mt-30'>{sloganApp} <i className='lni-chevron-right'></i></a>
                                 </div>
                             </div>
                         </div>
@@ -167,7 +172,7 @@ export default function Fluxo() {
                                     <div className='col-md-6 col-sm-8'>
                                         <div className='single-services text-center mt-30 borda'>
                                             <div className='services-icon'>
-                                                <i className='lni-customer'></i>
+                                                <i className='fas fa-smile-wink'></i>
                                             </div>
 
                                             <div className='services-content mt-20'>
@@ -178,7 +183,7 @@ export default function Fluxo() {
 
                                         <div className='single-services text-center mt-30 borda'>
                                             <div className='services-icon'>
-                                                <i className='lni-grid-alt'></i>
+                                                <i className='fas fa-users'></i>
                                             </div>
 
                                             <div className='services-content mt-20'>
@@ -191,7 +196,7 @@ export default function Fluxo() {
                                     <div className='col-md-6 col-sm-8'>
                                         <div className='single-services text-center mt-30 borda'>
                                             <div className='services-icon'>
-                                                <i className='lni-ruler-pencil'></i>
+                                                <i className='fas fa-dollar-sign'></i>
                                             </div>
 
                                             <div className='services-content mt-20'>
@@ -202,7 +207,7 @@ export default function Fluxo() {
 
                                         <div className='single-services text-center mt-30 borda'>
                                             <div className='services-icon'>
-                                                <i className='lni-support'></i>
+                                                <i className='fas fa-question'></i>
                                             </div>
 
                                             <div className='services-content mt-20'>
