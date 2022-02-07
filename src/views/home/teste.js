f28fbd80b073ba84530c0e5b88809a94f34af4c9
junioranheu <junioranheu@gmail.com>
2022-02-07 20:40:19 -0300

    aea

--- src/views/home/teste.js
+++ src/views/home/teste.js
@@ -1,10 +1,43 @@
-import React, { useEffect } from 'react';
+import NProgress from 'nprogress';
+import React, { useEffect, useState } from 'react';
+import { Aviso } from '../../componentes/outros/aviso';
+import '../../css/comentario.css';
+import '../../css/perfilEstabelecimento.css';
+import CONSTANTS_ESTABELECIMENTOS from '../../utilidades/const/constEstabelecimentos';
+import { Fetch } from '../../utilidades/utils/fetch';
 
 export default function SemAcesso() {
     // Ao carregar página;
     useEffect(() => {
         document.title = 'Fluxo — Teste'
     }, []);
+
+    async function getDetalheEstabelecimento() {
+        NProgress.start();
+
+        // Pegar o parâmetro da URL;
+        const url = `${CONSTANTS_ESTABELECIMENTOS.API_URL_GET_POR_ID}/49`;
+
+        let resposta = await Fetch.getApi(url);
+        if (resposta) {
+            setTeste(resposta);
+            NProgress.done();
+        } else {
+            Aviso.error('Algo deu errado ao consultar o estabelecimento em questão!', 5000);
+        }
+    }
+
+    const [teste, setTeste] = useState([]);
+    useEffect(() => {
+        // Pegar os detalhes do estabelecimento;
+        getDetalheEstabelecimento();
+    }, []);
+
+    useEffect(() => {
+        if (teste.usuarioId > 0) {
+            console.log('a');
+        }
+    }, [teste]);
 
     return (
         <section className='hero is-medium mt-6'>
