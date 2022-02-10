cd4a3f21046d672adec3f3a8d368ac0d53ad7921
junioranheu <junioranheu@gmail.com>
2022-02-10 16:26:44 -0300

    horarioBrasilia.js

--- src/utilidades/utils/horaBrasilia.js
+++ src/utilidades/utils/horaBrasilia.js
@@ -2,7 +2,8 @@ import moment from 'moment-timezone';
 
 const format = 'YYYY-M-D HH:mm:ss';
 const timezone = 'America/Sao_Paulo';
-const horarioBrasilia = moment.tz(moment(), format, timezone);
+// const horarioBrasilia = moment.tz(moment(), format, timezone);
+const horarioBrasilia = moment.tz(moment(), timezone).format(format);
 // console.log(horarioBrasilia);
 
 export default horarioBrasilia;
