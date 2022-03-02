6a93fb506fb26448dedbfc3a8ec5d76856a20d15
junioranheu <junioranheu@gmail.com>
2022-03-01 23:24:42 -0300

    Teste

--- src/utilidades/utils/horarioBrasilia.js
+++ src/utilidades/utils/horarioBrasilia.js
@@ -3,7 +3,7 @@ import Moment from 'moment-timezone';
 function horarioBrasilia() {
     const timezone = 'America/Sao_Paulo';
     Moment.tz.setDefault(timezone);
-    const horarioBrasilia = moment().tz();
+    const horarioBrasilia = Moment().tz();
     // console.log(horarioBrasilia);
     return horarioBrasilia;
 }
